d9869f616ee9801a0cfaf32251b485c27fcec8c6
ArthurLoboTrindade <arthurlobo.edu17@hotmail.com>
2025-11-20 19:25:01 -0300

    Configuraçao de Processamento Assincrono

---
+++ src/main/java/edu/ucsal/fiadopay/config/AsyncConfiguration.java
@@ -0,0 +1,28 @@
+package edu.ucsal.fiadopay.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfiguration {
+    private static final int CORE_POOL_SIZE = 5;
+    private static final int MAX_POOL_SIZE = 10;
+    private static final int QUEUE_CAPACITY = 25;
+    private static final String THREAD_NAME_PREFIX = "FiadoPay-Async-";
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        executor.initialize();
+        return executor;
+    }
+}
